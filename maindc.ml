0b1e2b130becd8a6da33d179459d0ae3bbe8f279
shleewhite <leewhite128@gmail.com>
2018-02-04 15:37:41 -0800

    changed stack empty and eof print statements

--- maindc.ml
+++ maindc.ml
@@ -16,7 +16,7 @@ type binop_t = bigint -> bigint -> bigint
 
 let print_number number = printf "%s\n%!" (string_of_bigint number)
 
-let print_stackempty () = printf "stack empty\n%!"
+let print_stackempty () = printf "dc: stack empty\n%!"
 
 let executereg (thestack: stack_t) (oper: char) (reg: int) =
     try match oper with
@@ -63,7 +63,7 @@ let toploop (thestack: stack_t) inputchannel =
                  | Operator oper       -> execute thestack oper
                  );
              toploop ()
-        with End_of_file -> printf "End_of_file\n%!";
+        with End_of_file -> printf "";
     in  toploop ()
 
 let readfiles () =
