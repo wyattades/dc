72e44ec65412aab063c8e053cfbf5c6bba3e8378
Wyatt Ades <wyattades@gmail.com>
2018-02-05 01:29:22 -0800

    added load and store registers

--- maindc.ml
+++ maindc.ml
@@ -11,6 +11,8 @@ type stack_t = Bigint.bigint Stack.t
 let push = Stack.push
 let pop = Stack.pop
 
+let regs = Array.make 256 []
+
 let ord thechar = int_of_char thechar
 type binop_t = bigint -> bigint -> bigint
 
@@ -18,10 +20,18 @@ let print_number number = printf "%s\n%!" (string_of_bigint number)
 
 let print_stackempty () = printf "dc: stack empty\n%!"
 
+let storereg (thestack: stack_t) (reg: int) =
+    regs.(reg) <- [ (pop thestack) ]
+
+let loadreg (thestack: stack_t) (reg: int) =
+    let vals = regs.(reg) in
+    if vals = [] then printf "dc: register '%c' (0%o) is empty\n" (char_of_int reg) reg
+    else push (List.hd vals) thestack
+
 let executereg (thestack: stack_t) (oper: char) (reg: int) =
     try match oper with
-        | 'l' -> printf "operator l reg 0%o is unimplemented\n%!" reg
-        | 's' -> printf "operator s reg 0%o is unimplemented\n%!" reg
+        | 'l' -> loadreg thestack reg
+        | 's' -> storereg thestack reg
         | _   -> printf "0%o 0%o is unimplemented\n%!" (ord oper) reg
     with Stack.Empty -> print_stackempty()
 
